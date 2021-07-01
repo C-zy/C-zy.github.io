1aa5aa7b11236f792919a592c17628f8d6538288
C-zy <1900696110@qq.com>
2021-07-01 18:48:44 +0800

    增加适配

--- APP/live2d/js/index.js
+++ APP/live2d/js/index.js
@@ -1,3 +1,17 @@
+// 移动端适配
+(function(doc, win) {
+  var docEl = doc.documentElement,
+      resizeEvt = 'orientationchange' in window ? 'orientationchange' : 'resize',
+      recalc = function() {
+          var clientWidth = docEl.clientWidth;
+          if (!clientWidth) return;
+          docEl.style.fontSize = 100 * (clientWidth / 750) + 'px';
+      };
+  if (!doc.addEventListener) return;
+  win.addEventListener(resizeEvt, recalc, false);
+  doc.addEventListener('DOMContentLoaded', recalc, false);
+})(document, window); 
+
 // time
 var myhour, myminute, mysecond;
 
